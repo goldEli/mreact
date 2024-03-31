aed478ee8f248351cf4b30e1a6202900d9761a36
miaoyu <miaoyu2009@qq.com>
2024-03-31 21:12:19 +0800

    feat: add test

---
+++ src/test/createElement.test.js
@@ -0,0 +1,59 @@
+import { test } from "vitest";
+import { describe } from "vitest";
+import React from "../react/React"
+import { expect } from "vitest";
+
+describe("create element" , () => {
+    test("base", () => {
+        const ele = React.createElement("div", { id: "app" }, "hi- ", "mini-react");
+        
+        expect(ele).toMatchInlineSnapshot(`
+         {
+               "props": {
+                 "children": [
+                   {
+                     "props": {
+                       "children": [],
+                       "nodeValue": "hi- ",
+                     },
+                     "type": "ELEMENT_TEXT",
+                   },
+                   {
+                     "props": {
+                       "children": [],
+                       "nodeValue": "mini-react",
+                     },
+                     "type": "ELEMENT_TEXT",
+                   },
+                 ],
+                 "id": "app",
+               },
+               "type": "div",
+             }
+        `)
+    })
+
+    test("props is null", () => {
+        const ele = React.createElement("div", null, "hi");
+        
+        expect(ele).toMatchInlineSnapshot(`
+         {
+               "props": {
+                 "children": [
+                   {
+                     "props": {
+                       "children": [],
+                       "nodeValue": "hi",
+                     },
+                     "type": "ELEMENT_TEXT",
+                   },
+                 ],
+               },
+               "type": "div",
+             }
+        `)
+    })
+
+    
+
+})
