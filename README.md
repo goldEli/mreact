b072e13ade61f6f89992d71dc1e84f70ef4cc52f
缪宇 <miaoyu2009@qq.com>
2024-04-01 11:13:46 +0800

    feat: add reconcilers

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 
-# createElement
+### createElement
 
 * element => virtual
 * createElement   type props children
+* createTextNode 
+
+### Reconcilers
+
+* render
+    * create dom (append dom)
+    * update props
+    * update children
+
+* requestIdleCallback
+    * 浏览器空闲时执行
+    * execute nextUnitOfWork
+    * nextUnitOfWork is fiber
+    * fiber is a linked list, which has child, sibling, return.
+    * create root fiber in render func
+        * props
+        * type
+        * child
+        * sibling
+        * return
+        * dom
+
--- demo/react/React.js
+++ demo/react/React.js
@@ -1,5 +1,4 @@
 const createTextNode = (nodeValue) => {
-    debugger
     return {
         type: "ELEMENT_TEXT",
         props: {
@@ -20,32 +19,126 @@ export const createElement = (type, props, ...children) => {
     }
 }
 export const render = (el, container) => {
-    console.log(el)
-    const dom = el.type === "ELEMENT_TEXT" ?
-        document.createTextNode('') :
-        document.createElement(el.type)
-
-    const { props } = el
-
-    // handle props
-    for (const key in props) {
-        if (key !== "children") {
-            dom[key] = props[key]
+    // create root filter
+    const rootFiber = {
+        sibling: null,
+        return: null,
+        child: el,
+        dom: container,
+        props: {
+            children: [el]
         }
     }
+    nextUnitOfWork = rootFiber
 
-    // handle children
-    const children = el.props?.children ?? [];
-    children.forEach((child) => {
-        render(child, dom);
-    });
-
-
-    container.append(dom)
+    requestIdleCallback(workLoop)
 }
 const React = {
     render,
     createElement,
     // createTextNode
 }
+
+function createDom(type) {
+
+    const dom = type === "ELEMENT_TEXT" ?
+        document.createTextNode('') :
+        document.createElement(type)
+    return dom
+}
+
+function handleProps(dom, props) {
+
+    Object.keys(props).forEach((key) => {
+        if (key === "children") {
+            return
+        }
+        dom[key] = props[key]
+    })
+}
+
+
+
+let nextUnitOfWork = null
+
+function performUnitOfWork() {
+    console.log(nextUnitOfWork)
+    const { props, type } = nextUnitOfWork
+
+    if (!nextUnitOfWork.dom) {
+        const dom = createDom(type)
+        nextUnitOfWork.dom = dom
+        nextUnitOfWork.return.dom.append(dom)
+    }
+
+
+    // handle props
+
+    handleProps(nextUnitOfWork.dom, props)
+
+    // handle children
+    const { children } = props
+    let prevChild = null
+    children.forEach((child, index) => {
+        const newFiber = {
+            return: nextUnitOfWork,
+            child: null,
+            sibling: null,
+            type: child.type,
+            props: child.props,
+            dom: null,
+        }
+        if (index === 0) {
+            nextUnitOfWork.child = newFiber
+        } else {
+            prevChild.sibling = newFiber
+        }
+        prevChild = newFiber
+    })
+
+    // return nextUnitOfWork
+    /**
+     * 先找儿子
+     * 没有儿子 找兄弟
+     * 没有兄弟 返回父亲 找父亲的兄弟
+     * 父亲没有兄弟 继续返回父亲的父亲
+     * until null
+     */
+
+    if (nextUnitOfWork.child) {
+        return nextUnitOfWork.child
+    }
+    if (nextUnitOfWork.sibling) {
+        return nextUnitOfWork.sibling
+    }
+
+    let parent = nextUnitOfWork.return
+    while (parent) {
+        if (parent.sibling) {
+            return parent.sibling
+        }
+        parent = parent.return
+    }
+
+    return null
+
+}
+
+function workLoop(deadline) {
+    // 执行工作单元
+    // 1. 执行当前工作单元
+    // 2. 判断当前工作单元是否还有剩余工作
+    // 3. 如果有剩余工作，则继续执行下一个工作单元
+
+    let isYield = false
+    // console.log(deadline.timeRemaining())
+    while (!isYield && !!nextUnitOfWork) {
+        nextUnitOfWork = performUnitOfWork()
+        isYield = !(deadline.timeRemaining() > 0)
+    }
+
+    requestIdleCallback(workLoop)
+}
+
+
 export default React
